3bde56a2c8a8f9202b2bfe199dd044395bee7fe5
Sean Shaw <2915054212@qq.com>
2025-08-16 19:14:45 +0800

    new with a structure

---
+++ Chapter_04/4.22 delete.cpp
@@ -0,0 +1,30 @@
+// delete.cpp -- using the delete operator
+#include <iostream>
+#include <cstring>
+using namespace std;
+char * getname(void);   // function prototype
+int main()
+{
+    char * name;        // create pointer but no storage
+
+    name = getname();   // assign address of string to name
+    cout << name << " at " << (int *)name <<"\n";
+    delete [] name;     // memory freed
+
+    name = getname();   // reuse freed memory
+    cout << name << " at " << (int *)name <<"\n";
+    delete [] name;     // memory freed again
+
+    return 0;
+}
+
+char * getname()
+{
+    char temp[80];
+    cout << "Enter last name: ";
+    cin >> temp;
+    char * pn = new char[strlen(temp) + 1]; 
+    strcpy(pn, temp);       // copy string into smaller space                          
+
+    return pn;              // temp lost when function ends
+}
