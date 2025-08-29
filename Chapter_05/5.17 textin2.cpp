8f1e8d8278dae96d107ec26e703709566b022bbf
Sean Shaw <2915054212@qq.com>
2025-08-29 17:43:42 +0800

    text in

---
+++ Chapter_05/5.17 textin2.cpp
@@ -0,0 +1,19 @@
+// textin2.cpp -- reading chars with a while loop
+#include <iostream>
+int main()
+{
+    using namespace std;
+    char ch;
+    int count = 0;      // use basic input
+    cout << "Enter characters; enter # to stop:\n";
+    cin.get(ch);
+    while (ch != '#')
+    {
+        cout << ch;
+        ++count;
+        cin.get(ch);
+    }
+    cout << endl << count << " characters read\n";
+
+    return 0;
+}
