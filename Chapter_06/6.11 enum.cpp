49dab1835c87d90822c9c0bfde1f874667cf006c
Sean Shaw <xiaozhuo0893@outlook.com>
2026-01-23 15:09:24 +0800

    continue and break

---
+++ Chapter_06/6.11 enum.cpp
@@ -0,0 +1,52 @@
+// enum.cpp -- uisng enum
+#include <iostream>
+// create named constants for 0 - 6
+enum {red, orange, yellow, green, blue, violet, indigo}; // 0-6
+int main()
+{
+    using namespace std;
+    cout << "Enter color code (0-6): ";
+    int code;
+    cin >> code;
+    while (code >= red && code <= indigo)
+    {
+        switch (code)
+        {
+        case red:
+            cout << "Her lisp were red.\n";
+            break;
+        
+        case orange:
+            cout << "Her hair was orange.\n";
+            break;
+        
+        case yellow:
+            cout << "Her shoes were yellow.\n";
+            break;
+        
+        case green:
+            cout << "Her nails were green.\n";
+            break;
+        
+        case blue:
+            cout << "Her sweatsuit were blue.\n";
+            break;
+        
+        case violet:
+            cout << "Her eyes were violet.\n";
+            break;
+        
+        case indigo:
+            cout << "Her mood was indigo.\n";
+            break;
+            
+        default:
+            break;
+        }
+        cout << "Enter color code (0-6): ";
+        cin >> code;
+    }
+    cout << "Bye!\n";
+
+    return 0;
+}
